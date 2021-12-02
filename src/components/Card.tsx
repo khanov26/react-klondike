6bcf02c9300618e98658b88f77c17e61d0763777
linar <khanov26@gmail.com>
2021-12-02 18:55:10 +0300

    display cards as svg

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,14 +6,29 @@ interface Props {
     card: ICard;
 }
 
+const reqSvgs = require.context('!@svgr/webpack!../assets/images/cards', false, /\.svg$/);
+const paths = reqSvgs.keys();
+const svgs = paths.reduce((images: Record<string, string>, path) => {
+    let match = path.match(/\.\/([\da-z]+)\.svg/i);
+    if (match !== null) {
+        const cardName = match[1];
+        images[cardName] = reqSvgs(path).default;
+    }
+    return images;
+}, {});
+
 const Card: React.FC<Props> = ({card}) => {
     const cardShortName = getShortName(card);
-    const cardImage = card.isUpturned ?
-        require(`../assets/images/cards/${cardShortName}.svg`).default :
-        require('../assets/images/cards/1B.svg').default;
+
+    const CardImage = card.isUpturned ?
+        svgs[cardShortName] :
+        svgs['1B'];
 
     return (
-        <img src={cardImage} className="card" alt=""/>
+        <div className="card">
+            <CardImage/>
+        </div>
+
     );
 };
 
